057b73180ae3d3aa8960b4f6be171f508f7bd942
Ines de Palma <ipalmamasaveu@gmail.com>
2021-11-10 23:25:38 +0100

    formSpree

--- src/components/form.js
+++ src/components/form.js
@@ -1,27 +1,38 @@
 import React from "react"
+import { useForm, ValidationError } from '@formspree/react';
 
 const Form = () => {
+  const [state, handleSubmit] = useForm("mayaryoo");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
-      <p>The form has a data-netlify="true" attribute, which tells Netlify to
-      register the form while building your site. The form has a name attribute
-      describing it. This is the name Netlify will give the form when you deploy
-      this code. The form’s name attribute is repeated in a hidden form-name
-      field. This is absolutely necessary. If you omit this field or mistype the
-      name, your entries will either throw an error or get lost somewhere in the
-      internet abyss. Every field has a name attribute. A field must have a name
-      for that data to be persisted within Netlify.
-      </p>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
     </form>
   )
 }
